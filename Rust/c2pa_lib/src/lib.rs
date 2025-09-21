3d342ad7edd94cb909aa53187182f1d50cf4e772
yag <kitakayaguchi@gmail.com>
2025-09-22 00:07:42 +0900

    Added runtime

--- Rust/c2pa_lib/src/lib.rs
+++ Rust/c2pa_lib/src/lib.rs
@@ -5,6 +5,19 @@ use jni::{
 };
 use once_cell::sync::Lazy;
 use std::sync::Mutex;
+use android_logger::Config;
+use log::{info, warn, error, LevelFilter};
+
+use std::sync::OnceLock;
+use tokio::runtime::Runtime;
+use tokio::time::{sleep, Duration};
+
+static RUNTIME: OnceLock<Runtime> = OnceLock::new();
+
+fn get_runtime() -> &'static Runtime {
+    RUNTIME.get_or_init(|| Runtime::new().expect("Tokio runtime failed"))
+}
+
 
 /// アプリ全体で共有したい状態
 struct AppState {
@@ -26,9 +39,31 @@ pub extern "system" fn Java_com_example_c2pajni_NativeBridge_callRustFunction(
     _class: JClass,
     input: JString,
 ) -> jstring {
+
+    // 一度だけ初期化
+    android_logger::init_once(
+        Config::default()
+            .with_max_level(LevelFilter::Debug) // 出力したいレベル
+            .with_tag("MyRustLib"),             // logcat でのタグ
+    );
+
     let text: String = env.get_string(&input).unwrap().into();
     let mut state = GLOBAL_STATE.lock().unwrap();
     state.counter += 1;
+
+    // Runtime を安全に取得
+    let rt = get_runtime();
+
+    // 非同期タスクを起動
+    let handle = rt.spawn(async move {
+        sleep(Duration::from_secs(10)).await;
+        info!("Rust spawn!");
+
+        format!("Hello from async, got: {}", "async")
+    });
+
+    // 結果をブロックして取得
+    //let result = rt.block_on(handle).unwrap();
 
     let reply = format!("Rust got '{}', counter = {}", text, state.counter);
     env.new_string(reply).unwrap().into_raw()
